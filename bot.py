56e3e02069ad82b64788b0bdceefa8139c868cd2
mrx <meibrohimov@gmail.com>
2026-05-20 13:10:17 +0500

    new changed

--- bot.py
+++ bot.py
@@ -5,27 +5,36 @@ from service import chatbot
 from dotenv import load_dotenv
 from aiogram.types import Message
 from aiogram import Bot, Dispatcher, F
-from database import create_table_users, add_user, increment_ai_count, check_user
-from aiogram.filters.command import CommandStart
+from aiogram.filters.command import CommandStart, CommandObject
+from database import create_table_users, add_user, increment_ai_count, check_user, get_connection, get_referral_link
 
 load_dotenv()
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def cmd_start(message: Message):
-    if check_user(message.from_user.id) == False:
-        add_user(message.from_user.full_name, message.from_user.username, message.from_user.id)
-        await message.answer("Assalomu alaykum!")
-    else:
-        await message.answer("Assalomu alaykum sizni yana bir bor ko'ranimizdan mamnummiz:)")
+async def start_handler(message: Message, command: CommandObject):
+    referred_by = None
+    if command.args:
+        referral_code = command.args
+        with get_connection() as conn:
+            cursor = conn.execute("""SELECT user_id FROM users WHERE referral_code = ?""", (referral_code,))
+            ref_user = cursor.fetchone()
+            if ref_user and ref_user[0] != message.from_user.id:
+                referred_by = ref_user[0]
+    if not check_user(message.from_user.id):
+        add_user(fullname=message.from_user.full_name, username=message.from_user.username,
+                 user_id=message.from_user.id, referred_by=referred_by)
+    await message.answer("Botga xush kelibsiz")
 
 
 @dp.message(F.text)
 async def ai_chatbot(message: Message):
     blocked = increment_ai_count(message.from_user.id)
     if blocked:
-        await message.answer("Limit tugagan (10/10). Premium kerak.")
+        referral_link = get_referral_link(message.from_user.id, "zaminclass8_bot")
+        await message.answer(
+            f"""Limit tugadi (10/10) 1 ta odam taklif qiling va limit reset bo'ladi.\nReferral:{referral_link}""")
     else:
         sent_message = await message.answer("...")
 
--- database.py
+++ database.py
@@ -14,30 +14,63 @@ def get_connection():
     finally:
         conn.close()
 
+def generate_referral_code(user_id: int) -> str:
+    return f"ref_{user_id}"
+
+def get_referral_link(user_id: int, bot_username: str):
+    code = generate_referral_code(user_id)
+    return f"https://t.me/{bot_username}?start={code}"
+
 
 def create_table_users():
     with get_connection() as conn:
-        cursor = conn.execute('''
+        conn.execute('''
             CREATE TABLE IF NOT EXISTS users(
                 id INTEGER PRIMARY KEY,
                 fullname TEXT NOT NULL,
                 username TEXT UNIQUE,
                 user_id INTEGER NOT NULL UNIQUE,
-                ai_count INTEGER DEFAULT 0
+                ai_count INTEGER DEFAULT 0,
+                referral_code TEXT UNIQUE,
+                referred_by INTEGER DEFAULT NULL
             )
         ''')
-        return cursor
 
 
-def add_user(fullname, username, user_id):
+def add_user(fullname, username, user_id, referred_by=None):
     try:
+        referral_code = generate_referral_code(user_id)
+
         with get_connection() as conn:
-            cursor = conn.execute('''
-                INSERT INTO users(fullname, username, user_id) VALUES(?,?,?)''',
-                                  (fullname, username, user_id))
+            cursor = conn.execute("""
+                INSERT INTO users(
+                    fullname,
+                    username,
+                    user_id,
+                    referral_code,
+                    referred_by
+                )
+                VALUES (?, ?, ?, ?, ?)
+            """, (
+                fullname,
+                username,
+                user_id,
+                referral_code,
+                referred_by
+            ))
+
+            # referral bonus
+            if referred_by:
+                conn.execute("""
+                    UPDATE users
+                    SET ai_count = 0
+                    WHERE user_id = ?
+                """, (referred_by,))
+
             return cursor.lastrowid
-    except sqlite3.IntegrityError as e:
-        return ValueError(f"Integer xatoligi: {e}")
+
+    except sqlite3.IntegrityError:
+        return None
 
 
 def increment_ai_count(user_id: int) -> bool:
@@ -50,13 +83,19 @@ def increment_ai_count(user_id: int) -> bool:
             WHERE user_id = ?
         """, (user_id,))
 
-        conn.commit()
+        cursor.execute("""
+            SELECT ai_count
+            FROM users
+            WHERE user_id = ?
+        """, (user_id,))
 
-        cursor.execute(
-            "SELECT ai_count FROM users WHERE user_id = ?",
-            (user_id,)
-        )
-        count = cursor.fetchone()[0]
+        result = cursor.fetchone()
+
+        # user topilmasa
+        if result is None:
+            return False
+
+        count = result[0]
 
         return count >= 10
 
